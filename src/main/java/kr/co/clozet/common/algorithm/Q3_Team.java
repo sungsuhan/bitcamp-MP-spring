c0a6af83d29838e3a77783d1c4a33908d0bd80ac
sungsuhan <swhanssu@gmail.com>
2022-05-10 00:41:04 +0900

    2022-05-10 00:40 1조

--- src/main/java/kr/co/clozet/common/algorithm/Q3_Team.java
+++ src/main/java/kr/co/clozet/common/algorithm/Q3_Team.java
@@ -18,10 +18,10 @@ public class Q3_Team {
         Scanner scanner = new Scanner(System.in);
         while (true){
             System.out.println("1.팀별 과제 2.팀장이 맡은 과제 3.큐를 담당한 사람 4.팀원별 과제 수");
-            String[] arr = {"권혜민","조현국","김진영","김한슬","서성민",
-                    "정렬","해시","힙","완전탐색","DP",
-                    "스택","깊이우선탐색","그래프","탐욕법","이중탐색",
-                    "큐","너비우선탐색"};
+            String[] arr = {"김승현",           "김윤섭",        "최민서",        "한성수",
+                            "Sort",             "Stack",        "Hash",        "Heap",
+                            "Binary Search",    "Queue",        "Graph",       "DFS",
+                            "Brute Force",       "DP",         "Greedy",       "BFS"};
             switch (scanner.next()){
                 case "1": teamSubject(arr); break;
                 case "2": teamLeaderSubject(arr); break;
@@ -33,77 +33,63 @@ public class Q3_Team {
 
     static void teamSubject(String[] arr) {
 
-        System.out.println("1.팀별 과제");
+        System.out.println("Q1.팀별 과제를 출력하세요");
         String s = "";
-        for (int i = 0; i < arr.length; i++) {
-            if (i % 5 == 1) {
+        for (int i = 0; i< arr.length; i++) {
+            if (i%4==0) {
                 s += "\n";
             }
-            s += i + ":" + arr[i] + "\t";
+            s += i + "." + arr[i] + "\t";
         }
         System.out.println(s);
+
     }
     static void teamLeaderSubject (String[] arr){
-        System.out.println("2.팀장이 맡은 과제만 출력하세요. 예) 김진영, 힙, 그래프 ");
+        System.out.println("Q2.팀장이 맡은 과제를 출력하세요"); //예) 김윤섭 : Stack, Queue, DP
         String s = "";
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals("김진영")) {
-                for (int j = 0; j < 3; j++) {
-                    s += arr[i + j * 5] + ",";
+        for (int i = 0; i< arr.length; i++) {
+            if (arr[i].equals("김윤섭")) {
+                for (int j=0; j<4; j++) {
+                    s += arr[i + j*4] + "\t";
                 }
-                if (i == 17) {
-                    break;
-                }
+                if (i==15) {break;}
             }
         }
         System.out.println(s);
     }
     static void queueManager (String[] arr) {
-        System.out.println("3.큐를 담당한 사람을 출력하세요. 예) 큐를 담당한 사람: 권혜민 ");
+        System.out.println("Q3.선택한 과목의 사람을 출력하세요"); //예) 큐를 담당한 사람 : 김윤섭
         String s = "";
-        /**
-         * Q3. 입력한 과목의 인덱스를 추출하세요
-         * 5, 10, 15 -> 0
-         * 6, 11, 16 -> 1
-         * 7, 12, 17 -> 2
-         */
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i].equals("힙")) {
-                // System.out.println("큐의 인덱스 : "+i);
-                s = arr[i] + "의 담당자는 " + arr[i % 5];
+        for (int i = 0; i< arr.length; i++) {
+            if (arr[i].equals("Queue")) {
+                s += arr[i%4];
             }
-
         }
         System.out.println(s);
     }
+
     static void subjectCountPerPerson (String[] arr){
-        System.out.println("4.팀원별 과제 수를 출력하세요 "
-                +"예) 권혜민(3개), 조현국(3개), 김진영(2개), 김한슬(2개), 서성민(2개) ");
-
-
-        /**
-         *  String[] arr = {"권혜민",   "조현국",     "김진영",  "김한슬",  "서성민",
-         *                 "정렬",     "해시",        "힙",   "완전탐색", "DP",
-         *                 "스택",  "깊이우선탐색 ",  "그래프", "탐욕법", "이중탐색",
-         *                 "큐",    "너비우선탐색"                        };
-         *   4 * 5
-         * */
+        System.out.println("Q4.팀원별 과제 수를 출력하세요"); //예) 김승현(3개) 김윤섭(3개) 최민서(3개) 한성수(3개)
 
         String s = "";
-        int[] intArr = new int[5];
-        int[] resArr = new int[5];
-        for(int i = 5; i< arr.length; i++){
-            int a = i % 5;
-            for(int j = 0; j<5; j++){
-                if(a == j){
+        int[] intArr = new int[4];
+        int[] resArr = new int[4];
+
+        for (int i=4; i<arr.length; i++) {
+            int a = i%4;
+            for (int j=0; j<4; j++) {
+                if (a==j) {
                     intArr[j]++;
                 }
                 resArr = intArr;
             }
         }
-        for(int i = 0; i< 5; i++){
-            System.out.println(arr[i]+"("+resArr[i]+"), ");
+        for(int i = 0; i < 4; i++){
+            System.out.println(arr[i]+"("+resArr[i]+"개)");
         }
+
     }
+
+
 
 }
